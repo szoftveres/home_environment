9a78cfe1a937ec383cfc2926c63bba9334771221
szoftveres <szoftveres@email.com>
2017-02-15 16:16:17 -0800

    Added install_software.bash

---
+++ install_software.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# bash
+apt-get install     bash vim
+
+# dev-general
+apt-get install     cscope ctags gcc g++ git gdb
+
+# dev-avr
+apt-get install     gcc-avr avr-libc avrdude
+
+# gui-graphics
+apt-get install     dia shotwell gimp
+
+# gui-audio
+apt-get install     audacity vlc pavucontrol
+
+# gui-office
+apt-get install     libreoffice
+
+# gui-dev
+apt-get install     meld wireshark
+
+# gui-internet
+apt-get install     pidgin firefox
+
+# gui-game
+apt-get install     dosbox
+
+
+
